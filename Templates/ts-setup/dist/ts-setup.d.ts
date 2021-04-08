567f1b1eff42d81f055f26f08cc384b7bbe85c12
macizomedia <info@macizomedia.com>
2021-04-08 12:01:33 +0200

    api-extractor setup

---
+++ Templates/ts-setup/dist/ts-setup.d.ts
@@ -0,0 +1,19 @@
+/**
+ * @packageDocumentation A small library for common math functions
+ */
+
+
+/**
+ * Calculate the average of three numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ * @param c - third number
+ *
+ * @public
+ */
+export declare function avg(a: number, b: number, c: number): number;
+
+/* Excluded from this release type: sum3 */
+
+export { }
