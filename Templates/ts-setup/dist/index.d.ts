1e3078f8254c2acdf080c168a3bfb27d46874685
macizomedia <info@macizomedia.com>
2021-04-08 11:48:12 +0200

    first commit

---
+++ Templates/ts-setup/dist/index.d.ts
@@ -0,0 +1,23 @@
+/**
+ * @packageDocumentation A small library for common math functions
+ */
+/**
+ * Calculate the average of three numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ * @param c - third number
+ *
+ * @public
+ */
+export declare function avg(a: number, b: number, c: number): number;
+/**
+ * Calculate the sum of three numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ * @param c - third number
+ *
+ * @beta
+ */
+export declare function sum3(a: number, b: number, c: number): number;
